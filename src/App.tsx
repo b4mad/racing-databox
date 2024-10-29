e7e932e18ee3af35293b559b21c1d0fe2de82f20
Marcel Hild <hild@b4mad.net>
2024-10-29 22:41:48 +0100

    mui box

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
+import { Box } from '@mui/material'
 import { Map } from './components/Map'
 
 function App() {
   return (
-    <div className="App">
+    <Box
+      sx={{
+        width: '100vw',
+        height: '100vh',
+        overflow: 'hidden'
+      }}
+    >
       <Map />
-    </div>
+    </Box>
   )
 }
 
