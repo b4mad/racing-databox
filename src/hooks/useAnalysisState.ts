1f6443f6f8299d423727ee6061d8409fd4a3430f
Marcel Hild <hild@b4mad.net>
2024-11-19 00:29:37 +0100

    refactor: Remove unused useEffect import in useAnalysisState hook
    Signed-off-by: Marcel Hild <hild@b4mad.net>

--- src/hooks/useAnalysisState.ts
+++ src/hooks/useAnalysisState.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { DelimitedNumericArrayParam, useQueryParam } from 'use-query-params';
 import { AnalysisData, TelemetryCacheEntry } from '../services/types';
 import { useZoomState } from './useZoomState';
